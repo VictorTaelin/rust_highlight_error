75023cde34a9ef9530abd34b2c9797fd23064488
FranchuFranchu <fff999abc999@gmail.com>
2022-07-31 09:15:29 -0300

    Add no_std support.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,10 @@
+#![cfg_attr(not(feature = "std"), no_std)]
+//! Features:
+//! - `std`: enabled by default. Enables std support. When it is disabled, the crate is `no_std`.
+//! It will still need `alloc` though.
+
+extern crate alloc;
+
 mod highlight_error;
 
 pub use highlight_error::{*};
